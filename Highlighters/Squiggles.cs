073f4b5b1563ed938926e30f5e38cd08454a130c
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-09-05 16:59:29 +0200

    Add comments.

--- Highlighters/Squiggles.cs
+++ Highlighters/Squiggles.cs
@@ -19,6 +19,7 @@ namespace Highlighters
         {
             test;
             /*warning*/ new Squiggles(11111111/*error*/);
+            // respeller
             // shamsutdinova
             string memberInList = "";
             /*hint*/ field = 1;
@@ -27,15 +28,16 @@ namespace Highlighters
         }
     }
 
+    // several squiggles on one member
     class person
     {
         [NotNull]
-        public string Name { get; set; }
+        public string Name { get; set; }// incorrect formatting
         [NotNull]
         public string PlaceOfBirth { get; set; }
 
         // Warning: Member with '[NotNull]' attribute is not initialized
-        public Person([NotNull] string name, string placeOfBirth)
+        public person([NotNull] string name, string placeOfBirth)
         {
             Name = name;
             Console.WriteLine(@"Name: {0}, Place of birth: {1}",
